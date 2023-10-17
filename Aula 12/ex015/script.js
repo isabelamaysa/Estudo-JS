39af8aded53529e1d8ef925054197f5af60f92fc
Isabela Maysa Souza Correa <isabelamaysa2002@gmail.com>
2023-10-17 14:46:37 -0300

    att ft

--- Aula 12/ex015/script.js
+++ Aula 12/ex015/script.js
@@ -45,8 +45,6 @@ function verificar() {
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
         res.appendChild(img)
-        
-        
     }
 
 }
