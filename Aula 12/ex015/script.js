e0f2a952c927f2f3cd2798810767a64eee27e517
Isabela Maysa Souza Correa <isabelamaysa2002@gmail.com>
2023-10-17 13:16:53 -0300

    att imagens

--- Aula 12/ex015/script.js
+++ Aula 12/ex015/script.js
@@ -8,7 +8,46 @@ function verificar() {
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        res.innerHTML = `Idade calculada: ${idade}`
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >=0 && idade < 10){
+                //criança
+                img.setAttribute('src', 'meninocrianca.png')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'homemjovem.png')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'homemadulto.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'homemidoso.png')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >=0 && idade < 10){
+                //criança
+                img.setAttribute('src', 'meninacrianca.png')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'mulherjovem.png')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'mulheradulta.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'mulheridosa.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+        
+        
     }
+
 }
  
